47dacc9e818fb384f9d7752cee69845d93679e3f
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-15 10:08:22 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- https://icanhazip.com);
-IZIN=$(curl https://raw.githubusercontent.com/Dork96/rentScript/main/ipvps | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/dpvpn09/ipvps/main/ipvps | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e ""
@@ -13,7 +13,7 @@ clear
 echo -e ""
 echo -e "======================================="
 echo -e "${red}=====[ Permission Denied...!!! ]=====${NC}";
-echo -e "Contact WA https//wa.me/+6285717614888"
+echo -e "Contact WA https//link.dpvpn.tk/wa"
 echo -e "For Registration IP VPS"
 echo -e "======================================="
 echo -e ""
